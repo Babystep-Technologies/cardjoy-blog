6dcb5948b518db1bd9c831c192d725ad8a491531
Marshall Shen <shen.marshall@gmail.com>
2025-07-06 16:09:21 -0500

    1% daily improvement

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'CardJoy Blog';
-export const SITE_DESCRIPTION = 'Blog on cardjoy.app, best digital greeting cards for any occasion.';
+export const SITE_TITLE = 'cardjoy blog';
+export const SITE_DESCRIPTION = 'blog on cardjoy.app, best digital greeting cards for any occasion.';
